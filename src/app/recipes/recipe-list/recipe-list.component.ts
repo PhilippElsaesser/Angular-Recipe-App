6476b18bdb52ce082f39dcc5ad67175776cd9b45
Philipp Elsässer <philipp-elsaesser@outlook.com>
2019-01-19 15:34:45 +0100

    created template to display recipes in recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
+import { RecipesComponent } from '../recipes.component';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      'Veggie Burger',
+      'A vegetarian Burger with grilled Eggplant',
+      'https://images.unsplash.com/photo-1512152272829-e3139592d56f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+      ),
+  ];
+  constructor() {}
 
   ngOnInit() {
   }
