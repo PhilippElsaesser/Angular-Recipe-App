b6c807493fe17a45a395b370eada59327a1fb802
Philipp Elsässer <philipp-elsaesser@outlook.com>
2019-01-20 00:15:36 +0100

    linked input fields in shopping list to add new items to list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @ViewChild('unitInput') unitInputRef: ElementRef;
+  @Output() newListitemCreated = new EventEmitter<Ingredient>();
 
-  constructor() { }
-
-  ngOnInit() {
+  createListitem() {
+    const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;
+    const unit = this.unitInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(name, amount, unit);
+    this.newListitemCreated.emit(newIngredient);
   }
-
 }
