68aee6345cea56338bc5a87523ecbb40bdaa807d
Philipp Elsässer <philipp-elsaesser@outlook.com>
2019-01-21 09:22:49 +0100

    enabled adding of ingredients to shopping-list from recipe detail section

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,4 +10,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+
+  constructor(private shoppligListService: ShoppingListService) {}
+
+  onAddToShoppingList() {
+    this.recipe.ingredients.map(
+      (ingredient: Ingredient) => {
+        this.shoppligListService.addIngredientToList(ingredient);
+      }
+    );
+  }
 }
