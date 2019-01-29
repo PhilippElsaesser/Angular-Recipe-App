b04d86430ef6548e7e621a571c2bf671ebadb73d
Philipp Elsässer <philipp-elsaesser@outlook.com>
2019-01-29 10:55:56 +0100

    added child-routes to recipes section, added recipe-edit component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,17 +1,33 @@
-import { ShoppingListService } from './../../shopping-list/shopping-list.service';
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
 import { Recipe } from '../recipe.model';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { RecipeService } from '../recipe.service';
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
-  @Input() recipe: Recipe;
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
 
-  constructor(private shoppligListService: ShoppingListService) {}
+  constructor(
+    private shoppligListService: ShoppingListService,
+    private route: ActivatedRoute,
+    private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    );
+  }
 
   onAddToShoppingList() {
     this.recipe.ingredients.map(
